6d114ab6d0c1db965d3c8afd6eb4d60749b1f92f
yash <yashgulati.g1@gmail.com>
2016-12-05 11:48:35 +0530

     

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-
-minutes = 25;
-seconds = 0;
+var minutes = 25;
+var seconds = 0;
+var clock;
+var running = 0;
 function back(){
   if(seconds == 0){ seconds = 59; minutes--; } else seconds--;
   $('#time').html( minutes+":"+seconds );
-
 }
 
 $(document).ready(start);
 
 function start() {
-    $('#clock').click(function(){
-      setInterval(back,1000);
-    });
+  $('#clock').click(function(){
+    if (!running){
+      running = 1;
+      clock = setInterval(back,1000);
+    }
+    else{
+      running = 0;
+      clearInterval(clock);
+    }
+  });
 
 }
